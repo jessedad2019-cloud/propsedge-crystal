1015872c4d44b0135e33f5e13788f46f004c5de9
jessedad2019-cloud <jessedad2019@gmail.com>
2025-10-15 22:00:21 -0400

    Connected client to live API

--- client/main.js
+++ client/main.js
@@ -9,3 +9,7 @@ app.innerHTML = `
   <h1>PropsEdge Crystal is live!</h1>
   <p>Your frontend is now connected and ready for build.</p>
 `
+fetch(`${import.meta.env.VITE_API_BASE_URL}/api/test`)
+  .then(res => res.text())
+  .then(msg => console.log("API says:", msg))
+  .catch(err => console.error("API error:", err));
